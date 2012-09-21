40ab77e7d43a1dcc63fbb2c009de6b897a2d4dd1
ßingen <bingentxu@gmail.com>
2012-09-21 11:28:10 -0400

    beginning maps

--- businesses.php
+++ businesses.php
@@ -61,6 +61,7 @@ echo "      </div> \n"; // search bar
     <script type='text/javascript'>
     $('#search-button-map').on({
 	click: function () {
+	    document.getElementById(\"search_type\").value=\"map\";
 	    $('#$div').load(get_reload_url(), function() {
 		var map = L.map('map').setView([51.505, -0.09], 13);
 		L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
